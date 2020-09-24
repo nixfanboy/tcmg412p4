a560bc93c31ea068969510a30703e715d9ffb4df
Trent Spears <spears.trent@tamu.edu>
2020-09-24 17:32:37 -0500

    Placed parsed data into appropriate containers

--- project4.py
+++ project4.py
@@ -17,21 +17,52 @@ else:
 file = open("cache.log", "r", encoding="windows-1252")
 print('Beginning log inspection...')
 
-# Analysis
-days = {"Sunday": 0, "Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0}
+# Collect Info
+days = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0} # 0 = Monday, 1 = Tuesday, 2 = Wednesday, etc
 weeks = {}
 months = {}
 files = {}
 errors = 0
 redirects = 0
+total_requests = 0
 for line in file:
     lines = line.split(" ")
     if len(lines) < 10:
         continue
-    date = date = datetime.strptime(lines[3], "[%d/%b/%Y:%H:%M:%S")
+    total_requests += 1
+    date = datetime.strptime(lines[3], "[%d/%b/%Y:%H:%M:%S")
     filename = lines[6]
     response = lines[8]
 
+    days[date.weekday()] += 1
+
+    week_str = str(date.isocalendar()[1]) + " " + str(date.year)
+    if week_str in weeks:
+        weeks[week_str] += 1
+    else:
+        weeks[week_str] = 1
+
+    month_str = str(date.month) + " " + str(date.year)
+    print(month_str)
+    if month_str in months:
+        months[month_str] += 1
+    else:
+        months[month_str] = 1
+
+    if filename in files:
+        files[filename] += 1
+    else:
+        files[filename] = 1
+
+    if response[0] == "4":
+        errors += 1
+    elif response[0] == "3":
+        redirects += 1
+
 file.close()
+
+# Analyze Info
+most_accessed_file = ""
+least_accessed_file = ""
 
 # Output Info
